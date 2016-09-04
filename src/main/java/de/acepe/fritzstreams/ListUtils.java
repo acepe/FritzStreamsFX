1318af19a8526afef5bdffa9b301471a630df30a
Andre Ackermann <andre.a@gmx.de>
2016-09-04 19:31:03 +0200

    Playlist entries can now be copied to clipboard and searched in google and VK

---
+++ src/main/java/de/acepe/fritzstreams/ListUtils.java
@@ -0,0 +1,59 @@
+package de.acepe.fritzstreams;
+
+import java.util.stream.Collectors;
+
+import javafx.event.EventHandler;
+import javafx.scene.control.ListView;
+import javafx.scene.input.*;
+import javafx.util.StringConverter;
+
+public class ListUtils {
+
+    /**
+     * Install the keyboard handler: CTRL + C = copy to clipboard
+     */
+    public static <T> void installCopyPasteHandler(ListView<T> table, StringConverter<T> converter) {
+        table.setOnKeyPressed(new KeyEventHandler<>(converter));
+    }
+
+    /**
+     * Copy/Paste keyboard event handler. The handler uses the keyEvent's source for the clipboard data. The source must
+     * be of type TableView.
+     */
+    public static class KeyEventHandler<T> implements EventHandler<KeyEvent> {
+        private final StringConverter<T> converter;
+        KeyCodeCombination copyKeyCodeCompination = new KeyCodeCombination(KeyCode.C, KeyCombination.CONTROL_ANY);
+
+        public KeyEventHandler(StringConverter<T> converter) {
+            this.converter = converter;
+        }
+
+        public void handle(final KeyEvent keyEvent) {
+            if (!copyKeyCodeCompination.match(keyEvent)) {
+                return;
+            }
+            if (keyEvent.getSource() instanceof ListView) {
+                // noinspection unchecked
+                copySelectionToClipboard((ListView<T>) keyEvent.getSource(), converter);
+                keyEvent.consume();
+            }
+        }
+    }
+
+    /**
+     * Get table selection and copy it to the clipboard.
+     */
+    public static <T> void copySelectionToClipboard(ListView<T> list, StringConverter<T> converter) {
+
+        String text = list.getSelectionModel()
+                          .getSelectedItems()
+                          .stream()
+                          .map(converter::toString)
+                          .collect(Collectors.joining("\n"));
+
+        final ClipboardContent clipboardContent = new ClipboardContent();
+        clipboardContent.putString(text);
+        Clipboard.getSystemClipboard().setContent(clipboardContent);
+    }
+
+}
--- src/main/java/de/acepe/fritzstreams/ui/PlaylistController.java
+++ src/main/java/de/acepe/fritzstreams/ui/PlaylistController.java
@@ -1,18 +1,35 @@
 package de.acepe.fritzstreams.ui;
 
+import com.sun.deploy.uitoolkit.impl.fx.HostServicesFactory;
+
 import de.acepe.fritzstreams.ControlledScreen;
+import de.acepe.fritzstreams.ListUtils;
 import de.acepe.fritzstreams.ScreenManager;
 import de.acepe.fritzstreams.backend.PlayListEntry;
 import de.acepe.fritzstreams.backend.Playlist;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.cell.TextFieldListCell;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 import javafx.util.StringConverter;
 
 public class PlaylistController implements ControlledScreen {
+
+    private static final String GOOGLE_TEMPLATE = "https://www.google.de/search?q=%s";
+    private static final String VK_TEMPLATE = "https://vk.com/audio?q=%s";
+    private static final String GOOGLE_ITEM_TEMPLATE = "Google nach \"%s\" durchsuchen";
+    private static final String VK_ITEM_TEMPLATE = "VKontakte nach \"%s\" durchsuchen";
+    private static final String COPY = "Kopieren";
 
     private final ObservableList<PlayListEntry> entriesList = FXCollections.observableArrayList();
 
@@ -20,15 +37,54 @@ public class PlaylistController implements ControlledScreen {
     private ListView<PlayListEntry> entriesListView;
     @FXML
     private Label titleLabel;
+    private ScreenManager screenManager;
 
     @FXML
     private void initialize() {
         entriesListView.setItems(entriesList);
-        entriesListView.setCellFactory(param -> new TextFieldListCell<>(entryConverter));
+        entriesListView.setCellFactory(param -> {
+            TextFieldListCell<PlayListEntry> cell = new TextFieldListCell<>(entryConverter);
+
+            ObjectProperty<PlayListEntry> itemProperty = cell.itemProperty();
+            StringProperty itemtextProperty = new SimpleStringProperty();
+            itemtextProperty.bindBidirectional(itemProperty, entryConverter);
+
+            MenuItem copyItem = new MenuItem(COPY);
+            copyItem.setOnAction(event -> {
+                ClipboardContent clipboardContent = new ClipboardContent();
+                clipboardContent.putString(entryConverter.toString(cell.getItem()));
+                Clipboard.getSystemClipboard().setContent(clipboardContent);
+            });
+
+            MenuItem googleItem = new MenuItem();
+            googleItem.textProperty().bind(Bindings.format(GOOGLE_ITEM_TEMPLATE, itemtextProperty));
+            googleItem.setOnAction(event -> searchTrack(GOOGLE_TEMPLATE, cell.itemProperty()));
+
+            MenuItem vkItem = new MenuItem();
+            vkItem.textProperty().bind(Bindings.format(VK_ITEM_TEMPLATE, itemtextProperty));
+            vkItem.setOnAction(event -> searchTrack(VK_TEMPLATE, cell.itemProperty()));
+
+            ContextMenu contextMenu = new ContextMenu();
+            contextMenu.getItems().addAll(copyItem, googleItem, vkItem);
+            cell.emptyProperty()
+                .addListener((obs, wasEmpty, isNowEmpty) -> cell.setContextMenu(isNowEmpty ? null : contextMenu));
+            return cell;
+        });
+        ListUtils.installCopyPasteHandler(entriesListView, entryConverter);
+    }
+
+    private void searchTrack(String template, ObjectProperty<PlayListEntry> itemProperty) {
+        String searchUri = String.format(template, itemToString(itemProperty));
+        HostServicesFactory.getInstance(screenManager.getApplication()).showDocument(searchUri);
+    }
+
+    private String itemToString(ObjectProperty<PlayListEntry> itemProperty) {
+        return entryConverter.toString(itemProperty.get());
     }
 
     @Override
     public void setScreenManager(ScreenManager screenManager) {
+        this.screenManager = screenManager;
     }
 
     public void setPlayList(Playlist playlist) {
@@ -39,6 +95,9 @@ public class PlaylistController implements ControlledScreen {
     private final StringConverter<PlayListEntry> entryConverter = new StringConverter<PlayListEntry>() {
         @Override
         public String toString(PlayListEntry entry) {
+            if (entry == null) {
+                return "";
+            }
             return entry.getArtist() + " - " + entry.getTitle();
         }
 
